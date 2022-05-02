fa1723a348d3c4d38594c975f019f1e360f7f9ad
elsuppo <andrew.suponev@gmail.com>
2022-05-02 23:30:15 +0300

    feat: complete remove from list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+// const { NotImplementedError } = require('../extensions/index.js');
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -22,9 +22,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  let currentList = l
+
+  while (currentList.value === k) {
+    currentList = currentList.next
+  }
+
+  let newList = currentList
+
+  while (newList.next) {
+    if (newList.next.value === k) {
+      newList.next = newList.next.next
+    }
+    else {
+      newList = newList.next
+    }
+  }
+  return currentList
 }
 
 module.exports = {
